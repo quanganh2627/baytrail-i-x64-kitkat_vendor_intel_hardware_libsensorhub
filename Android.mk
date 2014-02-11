9c66bf4ada2220e1d3d015d389b8732746cb8585
Guilhem IMBERTON <guilhem.imberton@intel.com>
2014-02-11 11:15:20 +0100

    [SDK] Removing eng debug and user flags from components makefiles
    BZ: 143553
    
    - Also adding makefile cleanup
    
    Change-Id: I3277001f141f24752ba97c85471e799861a361e2
    Signed-off-by: Guilhem IMBERTON <guilhem.imberton@intel.com>

--- Android.mk
+++ Android.mk
@@ -6,44 +6,25 @@ LOCAL_PATH := $(call my-dir)
 # sensorhubd - sensorhub daemon
 #
 include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
-
-LOCAL_SRC_FILES := src/daemon/main.c \
-			src/utils/utils.c
-
-LOCAL_SHARED_LIBRARIES := liblog libhardware_legacy
-
-#LOCAL_SHARED_LIBRARIES += libcontextarbitor
-
-#LOCAL_CFLAGS := -DENABLE_CONTEXT_ARBITOR
-
 LOCAL_MODULE := sensorhubd
-
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := \
+    src/daemon/main.c \
+    src/utils/utils.c
+LOCAL_SHARED_LIBRARIES := liblog libhardware_legacy
 include $(BUILD_EXECUTABLE)
 
 #
 # libsensorhub - sensorhub client library
 #
 include $(CLEAR_VARS)
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
-
-LOCAL_SRC_FILES := src/lib/libsensorhub.c \
-			src/utils/utils.c
-
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/src/include
-
 LOCAL_MODULE := libsensorhub
-
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := \
+    src/lib/libsensorhub.c \
+    src/utils/utils.c
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/src/include
 include $(BUILD_SHARED_LIBRARY)
 
 
@@ -51,81 +32,49 @@ include $(BUILD_SHARED_LIBRARY)
 # sensorhub_client - sensorhub test client
 #
 include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
-
-LOCAL_SRC_FILES := src/tests/sensor_hub_client.c
-
-LOCAL_SHARED_LIBRARIES += libsensorhub liblog
 LOCAL_MODULE := sensorhub_client
-
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := src/tests/sensor_hub_client.c
+LOCAL_SHARED_LIBRARIES := libsensorhub liblog
 include $(BUILD_EXECUTABLE)
 
 #
 # test.c from Alek
 #
 include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := eng
-
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
-
-LOCAL_SRC_FILES := src/tests/test.c
-
-# LOCAL_SHARED_LIBRARIES += libsensorhub
 LOCAL_MODULE := test_alek
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := src/tests/test.c
+include $(BUILD_EXECUTABLE)
 
 #
 # bist_test.c
 #
 include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := eng
-
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
-
+LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := src/tests/bist_test.c
-
-LOCAL_SHARED_LIBRARIES += libsensorhub
-
+LOCAL_SHARED_LIBRARIES := libsensorhub
 LOCAL_MODULE := bist_test
-
 include $(BUILD_EXECUTABLE)
 
 #
 # compass calibration test.
 #
 include $(CLEAR_VARS)
-
+LOCAL_MODULE := calibration
 LOCAL_MODULE_TAGS := optional
-
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
-
 LOCAL_SRC_FILES := src/tests/calibration.c
-
-LOCAL_SHARED_LIBRARIES += libsensorhub
-
-LOCAL_MODULE	:= calibration
-
+LOCAL_SHARED_LIBRARIES := libsensorhub
 include $(BUILD_EXECUTABLE)
 
 #
 # event notification test.
 #
 include $(CLEAR_VARS)
-
+LOCAL_MODULE := event_notification
 LOCAL_MODULE_TAGS := optional
-
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
-
 LOCAL_SRC_FILES := src/tests/event_notification.c
-
-LOCAL_SHARED_LIBRARIES += libsensorhub liblog
-
-LOCAL_MODULE    := event_notification
-
+LOCAL_SHARED_LIBRARIES := libsensorhub liblog
 include $(BUILD_EXECUTABLE)
 
 endif
